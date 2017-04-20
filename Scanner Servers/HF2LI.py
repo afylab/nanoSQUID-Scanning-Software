ab95d1018c3e8752191feeeeb5e655ee053ffa87
mserlin <marecserlin@gmail.com>
2017-04-20 14:45:58 -0700

    Add files via upload

---
+++ Scanner Servers/HF2LI.py
@@ -0,0 +1,233 @@
+# Copyright []
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+### BEGIN NODE INFO
+[info]
+name = Cryo Positioning Systems Controller (CPSC) Server
+version = 1.0
+description = Communicates with the CPSC which controls the JPE piezo stacks. 
+Must be placed in the same directory as cacli.exe in order to work. 
+### END NODE INFO
+"""
+
+from labrad.server import LabradServer, setting
+from twisted.internet.defer import inlineCallbacks, returnValue
+import labrad.units as units
+from labrad.types import Value
+import time
+import numpy as np
+import zhinst
+import zhinst.utils
+
+
+class HF2LIServer(LabradServer):
+    name = "HF2LI Server"    # Will be labrad name of server
+
+    #@inlineCallbacks
+    def initServer(self):  # Do initialization here
+        self.daq = None
+        self.dev_ID = None
+        self.device_list = None
+        self.props = None
+        print "Server initialization complete"
+        
+    @setting(100,returns = '')
+    def detect_devices(self,c):
+        """ Attempt to connect to the LabOne server (not a LadRAD server) and get a list of devices."""
+        try:
+            self.daq = yield zhinst.utils.autoConnect()
+            print 'LabOne DAQ Server detected.'
+            self.device_list = yield zhinst.utils.devices(self.daq)
+            print 'Devices connected to LabOne DAQ Server are the following:'
+            print self.device_list
+        except RuntimeError:
+            print ('Failed to detected LabOne DAQ Server and an associated Zurich Instruments device.'
+                ' Check that everything is plugged into the computer.')
+    
+    @setting(101,returns = '*s')
+    def get_device_list(self,c):
+        """Returns the list of devices. If none have been detected (either because detect_devices has not yet
+        been run, or because of a bad connection), this will return None. """
+        returnValue(self.device_list)
+            
+    @setting(102,dev_ID = 's', returns = '')
+    def select_device(self, c, dev_ID="None"):
+        """Sets the active device ID to the provided dev_ID. If no dev_ID is provided, sets the active
+        device to the first device from the device list."""
+        if dev_ID == "None":
+            self.dev_ID = self.device_list[0]
+            (self.daq, self.dev_ID, self.props) = zhinst.utils.create_api_session(self.dev_ID, 1)
+        else: 
+            if dev_ID in self.device_list:
+                self.dev_ID = dev_ID
+                (self.daq, self.dev_ID, self.props) = zhinst.utils.create_api_session(self.dev_ID, 1)
+            else:
+                print "Provided device ID is not in the list of possible devices."
+   
+    @setting(103,settings = '*s', returns = '')
+    def set_settings(self, c, settings):
+        """Simultaneously set all the settings described in the settings input. Settings should be a 
+            list of string and input tuples, where the string provides the node information and the
+            input is the required input. For example: 
+            setting =   [['/%s/demods/*/enable' % self.dev=ID, 0],
+                        ['/%s/demods/*/trigger' % self.dev=ID, 0],
+                        ['/%s/sigouts/*/enables/*' % self.dev=ID, 0],
+                        ['/%s/scopes/*/enable' % self.dev=ID, 0]]
+            This function allows changing multiple settings quickly, however it requires knowledge 
+            of the node names. Every setting that can be set through this function can also be 
+            set through other functions."""
+        yield daq.set(settings)
+        
+    @setting(104,returns = '')
+    def sync(self,c):
+        """Perform a global synchronisation between the device and the data server:
+            Ensure that the settings have taken effect on the device before setting
+            the next configuration."""
+        yield self.daq.sync()
+               
+    @setting(105,returns = '')
+    def disable_outputs(self):
+        """Create a base instrument configuration: disable all outputs, demods and scopes."""
+        general_setting = [['/%s/demods/*/enable' % self.dev_ID, 0],
+                           ['/%s/demods/*/trigger' % self.dev_ID, 0],
+                           ['/%s/sigouts/*/enables/*' % self.dev_ID, 0],
+                           ['/%s/scopes/*/enable' % self.dev_ID, 0]]
+        yield self.daq.set(general_setting)
+        # Perform a global synchronisation between the device and the data server:
+        # Ensure that the settings have taken effect on the device before setting
+        # the next configuration.
+
+        
+    @setting(106,input_channel = 'i', on = 'b', returns = '')
+    def set_ac(self, c, input_channel, on):
+        """Set the AC coupling of the provided input channel (1 indexed) to on, if on is True, 
+        and to off, if on is False"""
+        setting = ['/%s/sigins/%d/ac' % (self.dev_ID, input_channel-1), on],
+        yield self.daq.set(setting)
+        
+    @setting(107,input_channel = 'i', on = 'b', returns = '')
+    def set_imp50(self, c, input_channel, on):
+        """Set the input impedance of the provided input channel (1 indexed) to 50 ohms, if on is True, 
+        and to 1 mega ohm, if on is False"""
+        setting = ['/%s/sigins/%d/imp50' % (self.dev_ID, input_channel-1), on],
+        yield self.daq.set(setting)
+        
+    @setting(108,input_channel = 'i', amplitude = 'v[]', returns = '')
+    def set_range(self, c, input_channel, amplitude):
+        """Set the input voltage range of the provided input channel (1 indexed) to the provided amplitude in Volts."""
+        setting = ['/%s/sigins/%d/range' % (self.dev_ID, input_channel-1), amplitude],
+        yield self.daq.set(setting)
+        
+    @setting(109,input_channel = 'i', on = 'b', returns = '')
+    def set_diff(self, c, input_channel, on):
+        """Set the input mode of the provided input channel (1 indexed) to differential, if on is True, 
+        and to single ended, if on is False"""
+        setting = ['/%s/sigins/%d/diff' % (self.dev_ID, input_channel-1), on],
+        yield self.daq.set(setting)
+        
+    @setting(110,osc_index= 'i', freq = 'v[]', returns = '')
+    def set_oscillator_freq(self,c, osc_index, freq):
+        """Set the frequency of the designated oscillator (1 indexed) to the provided frequency. The HF2LI Lock-in has
+        two oscillators. """
+        setting = ['/%s/oscs/%d/freq' % (self.dev_ID, osc_index-1), freq],
+        yield self.daq.set(setting)
+        
+    @setting(111,demod_index= 'i', oscselect = 'i', returns = '')
+    def set_demod_osc(self,c, demod_index, oscselect):
+        """Sets the provided demodulator to select the provided oscillator as its reference frequency. The HF2LI Lock-in has
+        six demodulators and two oscillators."""
+        setting = ['/%s/demods/%d/oscselect' % (self.dev_ID, demod_index-1), oscselect-1],
+        yield self.daq.set(setting)
+        
+    @setting(112,demod_index= 'i', harm = 'i', returns = '')
+    def set_demod_harm(self,c, demod_index, harm):
+        """Sets the provided demodulator harmonic. Demodulation frequency will be the reference oscillator times the provided
+        integer harmonic."""
+        setting = ['/%s/demods/%d/harmonic' % (self.dev_ID, demod_index-1), harm],
+        yield self.daq.set(setting)
+        
+    @setting(113,demod_index= 'i', phase = 'v[]', returns = '')
+    def set_demod_phase(self,c, demod_index, phase):
+        """Sets the provided demodulator phase."""
+        setting = ['/%s/demods/%d/phaseshift' % (self.dev_ID, demod_index-1), phaseshift],
+        yield self.daq.set(setting)
+        
+    @setting(114,demod_index= 'i', input_channel = 'i', returns = '')
+    def set_demod_input(self,c, demod_index, input_channel):
+        """Sets the provided demodulator phase."""
+        setting = ['/%s/demods/%d/adcselect' % (self.dev_ID, demod_index-1), input_channel-1],
+        yield self.daq.set(setting)
+        
+    @setting(115,demod_index = 'i', rec_time= 'v[]', timeout = 'i', returns = '**v[]')
+    def poll_demod(self,c, demod_index, rec_time, timeout):
+        """This function returns subscribed data previously in the API's buffers or
+            obtained during the specified time. It returns a dict tree containing
+            the recorded data. This function blocks until the recording time is
+            elapsed. Recording time input is in seconds. Timeout time input is in 
+            milliseconds. Recommended timeout value is 500ms."""
+        
+        path = '/%s/demods/%d/sample' % (self.dev_ID, demod_index-1)
+        yield self.daq.flush()
+        yield self.daq.subscribe(path)
+        
+        ans = yield self.daq.poll(rec_time, timeout, 1, True)
+        
+        x_data = ans[path]['x']
+        y_data = ans[path]['y']
+        
+        yield self.daq.unsubscribe(path)
+        
+        returnValue([x_data, y_data])
+        
+    @setting(116,output_channel = 'i', on = 'b', returns = '')
+    def set_output(self, c, output_channel, on):
+        """Turns the output of the provided output channel (1 indexed) to on, if on is True, 
+        and to off, if on is False"""
+        setting = ['/%s/sigouts/%d/on' % (self.dev_ID, output_channel-1), on],
+        yield self.daq.set(setting)
+        
+    @setting(117,output_channel = 'i', amp = 'v[]', returns = '')
+    def set_output_amplitude(self, c, output_channel, amp):
+        """Sets the output amplitude of the provided output channel (1 indexed) to the provided input amplitude
+        in units of the output range."""
+        if output_channel == 1:
+            setting = ['/%s/sigouts/%d/amplitudes/6' % (self.dev_ID, output_channel-1), amp],
+        elif output_channel == 2:
+            setting = ['/%s/sigouts/%d/amplitudes/7' % (self.dev_ID, output_channel-1), amp],
+        yield self.daq.set(setting)
+        
+    @setting(118,output_channel = 'i', range = 'v[]', returns = '')
+    def set_output_range(self, c, output_channel, range):
+        """Sets the output range of the provided output channel (1 indexed) to the provided input amplitude
+        in units of volts. Will automatically go to the value just above the desired provided range. Sets to
+        10 mV, 100 mV, 1 V or 10V."""
+        setting = ['/%s/sigouts/%d/range' % (self.dev_ID, output_channel-1), range],
+        yield self.daq.set(setting)
+        
+    @setting(119,output_channel = 'i', returns = 'v[]')
+    def get_output_range(self, c, output_channel):
+        """Gets the output amplitude of the provided output channel (1 indexed) to the provided input amplitude
+        in units of the output range."""
+        setting = '/%s/sigouts/%d/range' % (self.dev_ID, output_channel-1)
+        dic = yield self.daq.get(setting,True)
+        range = float(dic[setting])
+        returnValue(range)
+        
+__server__ = HF2LIServer()
+
+if __name__ == '__main__':
+    from labrad import util
+    util.runServer(__server__)
