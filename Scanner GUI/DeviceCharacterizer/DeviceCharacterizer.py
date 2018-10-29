52213089c6a34c054ff0ffa2a7b487ae30125461
Ray <jiacheng_zhu@umail.ucsb.edu>
2018-10-29 15:53:52 -0700

    Add files via upload

--- Scanner GUI/DeviceCharacterizer/DeviceCharacterizer.py
+++ Scanner GUI/DeviceCharacterizer/DeviceCharacterizer.py
@@ -18,7 +18,8 @@ Ui_ServerList, QtBaseClass = uic.loadUiType(path + r"\requiredServers.ui")
 
 #Not required, but strongly recommended functions used to format numbers in a particular way. 
 sys.path.append(sys.path[0]+'\Resources')
-from nSOTScannerFormat import readNum, formatNum, processLineData, processImageData, ScanImageView
+#from nSOTScannerFormat import readNum, formatNum, processLineData, processImageData, ScanImageView                #this part not sure, it was throwing an error
+
 
 class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
     
@@ -34,8 +35,11 @@ class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
         self.dataProcessing = 'Raw'
         self.dataPostProcessing = 'Raw'
         
+        
         self.reactor = reactor
         self.setupUi(self)
+        
+        self.setupPlot()
         
         self.inputs = {
                 'Input 1'                : 1,         # 1 Indexed DAC input into which the voltage corresponding to the Z atto motion 
@@ -51,31 +55,63 @@ class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
         self.numberfastdata=100
         self.numberslowdata=100
         self.lineTime = 64e-3
-        self.delay = int(1e6 *self.lineTime / self.numberfastdata)
-        self.startInput1 = 0.0
-        self.startInput2 = 0.0
-        self.stopInput1=0.1
-        self.stopInput2=0.1
-        
 
-        self.setupAdditionalUi()
+
+
+        self.SweepChannel=int(0)
+        self.MinVoltage=float(-1)
+        self.MaxVoltage=float(1)
+        self.Numberofstep=int(1000)
+        self.Delay=int(10)
+
+#        self.setupAdditionalUi()
 
         self.moveDefault()
 
         #Connect show servers list pop up
         self.push_Servers.clicked.connect(self.showServersList)
+        
+#######################################
         self.comboBox_Input1.currentIndexChanged.connect(self.setInput1)
         self.comboBox_Output1.currentIndexChanged.connect(self.setOutput1)
         self.comboBox_Output2.currentIndexChanged.connect(self.setOutput2)
-        self.PushButton_StartSweep.clicked.connect(self.sweep)
+        self.PushButton_StartSweep.clicked.connect(self.Sweep)
+#######################################
+
+#######################################
+        self.Lineedit_MinVoltage.editingFinished.connect(self.UpdateMinVoltage)
+        self.Lineedit_MaxVoltage.editingFinished.connect(self.UpdateMaxVoltage)
+        self.Lineedit_Numberofstep.editingFinished.connect(self.UpdateNumberofstep)
+        self.Lineedit_Delay.editingFinished.connect(self.UpdateDelay)
+#######################################
+        # self.Lineedit_Channel.editingFinished.connect(self.UpdateChannel)
+
+
+        self.PushButton_done.clicked.connect(self.dummy)
+
         #Initialize all the labrad connections as none
         self.cxn = None
         self.dv = None
         
-        
+
 
         
         #self.lockInterface()
+
+        
+    def setupPlot(self):
+        self.sweepPlot = pg.PlotWidget(parent = self.PreliminaryPlot)
+        self.sweepPlot.setGeometry(QtCore.QRect(0, 0, 435, 290))
+        self.sweepPlot.setLabel('left', 'DC Feedback Voltage', units = 'V')
+        self.sweepPlot.setLabel('bottom', 'Bias Voltage', units = 'V')
+        self.sweepPlot.showAxis('right', show = True)
+        self.sweepPlot.showAxis('top', show = True)
+        self.sweepPlot.setXRange(0,1)
+        self.sweepPlot.setYRange(0,2)
+        self.sweepPlot.enableAutoRange(enable = True)
+        
+        
+        
         
     def moveDefault(self):
         self.move(550,10)
@@ -127,7 +163,12 @@ class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
 
         self.push_Servers.setStyleSheet("#push_Servers{" + 
             "background: rgb(144, 140, 9);border-radius: 4px;}")
-            
+
+
+        
+
+
+        
     def showServersList(self):
         serList = serversList(self.reactor, self)
         serList.exec_()
@@ -135,23 +176,24 @@ class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
     def updateDataVaultDirectory(self):
         curr_folder = yield self.gen_dv.cd()
         yield self.dv.cd(curr_folder)
-        
-    def setupAdditionalUi(self):
-        #Set up UI that isn't easily done from Qt Designer
-        self.view = pg.PlotItem(title="Plot")
-#        self.view.setLabel('left',text='Y position',units = 'm')
-#        self.view.setLabel('right',text='Y position',units = 'm')
-#        self.view.setLabel('top',text='X position',units = 'm')
-#        self.view.setLabel('bottom',text='X position',units = 'm')
-        self.Plot2D = ScanImageView(parent = self.centralwidget, view = self.view, randFill = self.randomFill)
-        self.view.invertY(False)
-        self.view.setAspectLocked(self.aspectLocked)
-        self.Plot2D.ui.roiBtn.hide()
-        self.Plot2D.ui.menuBtn.hide()
-        self.Plot2D.ui.histogram.item.gradient.loadPreset('bipolar')
-        self.Plot2D.lower()
-        self.PlotArea.close()
-        self.horizontalLayout.addWidget(self.Plot2D)
+
+     
+    # def setupAdditionalUi(self):
+        # #Set up UI that isn't easily done from Qt Designer
+        # self.view = pg.PlotItem(title="Plot")
+# #        self.view.setLabel('left',text='Y position',units = 'm')
+# #        self.view.setLabel('right',text='Y position',units = 'm')
+# #        self.view.setLabel('top',text='X position',units = 'm')
+# #        self.view.setLabel('bottom',text='X position',units = 'm')
+        # self.Plot2D = ScanImageView(parent = self.centralwidget, view = self.view, randFill = self.randomFill)
+        # self.view.invertY(False)
+        # self.view.setAspectLocked(self.aspectLocked)
+        # self.Plot2D.ui.roiBtn.hide()
+        # self.Plot2D.ui.menuBtn.hide()
+        # self.Plot2D.ui.histogram.item.gradient.loadPreset('bipolar')
+        # self.Plot2D.lower()
+        # self.PlotArea.close()
+        # self.horizontalLayout.addWidget(self.Plot2D)
         
     def updateHistogramLevels(self, hist):
         mn, mx = hist.getLevels()
@@ -172,23 +214,68 @@ class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
         self.outputs['Output 2'] = self.comboBox_Output2.currentIndex()+1
         print self.outputs
 
-    def sweep(self,c):
-        yield self.sleep(0.1)
-        
-        try: 
-            min=-1
-            max=1
-            range=abs(max-min)
-            delay=0.1
-            for folder in file_info[0][1:]:
-                session = session + '\\' + folder
-            self.lineEdit_ImageDir.setText(r'\.datavault' + session)    
 
-            dac_read=yield self.dac.buffer_ramp([0],[0],[min],[max],1000,delay)
+    @inlineCallbacks
+    def Sweep(self,c=None):
+        yield self.sleep(0.1)
+        a = yield self.dac.read()
+        while a != '':
+            print a
+            a = yield self.dac.read()
+
+        file_info = yield self.dv.new("Experimenting with DeviceCharacterizer", ['Voltage'],['Reading'])
+        self.dvFileName = file_info[1]
+        # self.lineEdit_ImageNum.setText(file_info[1][0:5])
+        session  = ''
+        for folder in file_info[0][1:]:
+            session = session + '\\' + folder
+        # self.lineEdit_ImageDir.setText(r'\.datavault' + session)
+            
+
         
-        except Exception as inst:
-            print inst
-    
+        # except Exception as inst:
+            # print inst
+        self.dac.ramp1(self.SweepChannel,0,self.MinVoltage,10000,100)    #ramp to initial value
+        print self.SweepChannel
+        print self.MinVoltage
+        print self.MaxVoltage
+        print self.Numberofstep
+        print self.Delay
+
+        dac_read= yield self.dac.buffer_ramp([self.SweepChannel],[0,1],[self.MinVoltage],[self.MaxVoltage],self.Numberofstep,self.Delay)
+        
+        formatted_data = []
+        for j in range(0, self.Numberofstep):
+            formatted_data.append((j, dac_read[0][j], dac_read[1][j]))
+        yield self.dv.add(formatted_data)
+        
+        
+        print self.SweepChannel
+        print self.MaxVoltage
+        self.dac.ramp1(self.SweepChannel,self.MaxVoltage,0.0,10000,100)
+        
+        print "done"
+        self.plotSweepData(formatted_data)
+
+    def dummy(self):
+        print self.MinVoltage
+        self.dac.set_voltage(self.SweepChannel,0)
+        print "done2"
+
+    def plotSweepData(self, data):
+        self.data = data
+        xVals = [x[1] for x in self.data]
+        print xVals
+        yVals = [x[2] for x in self.data]
+        print yVals
+        # try:
+        self.sweepPlot.plot(x = xVals, y = yVals, pen = 0.5)
+            
+        # except Exception as inst:
+            # print inst
+
+        
+        
     @inlineCallbacks
     def update_data(self):
         try:
@@ -224,13 +311,31 @@ class Window(QtGui.QMainWindow, DeviceCharacterizerWindowUI):
         except:
             print "This is an error message!"
 
+            
+##########################Update All the parameters#################
+    def UpdateMinVoltage(self):
+        self.MinVoltage=float(str(self.Lineedit_MinVoltage.text()))
+        print self.MinVoltage
+        
+    def UpdateMaxVoltage(self):
+        self.MaxVoltage=float(str(self.Lineedit_MaxVoltage.text()))
+        
+    def UpdateNumberofstep(self):
+        self.Numberofstep=int(str(self.Lineedit_Numberofstep.text()))
+ 
+    def UpdateDelay(self):
+        self.Delay=int(str(self.Lineedit_Delay.text()))
+            
+##########################Update All the parameters#################
+
         
     def updateHistogramLevels(self, hist):
         mn, mx = hist.getLevels()
         self.Plot2D.ui.histogram.setLevels(mn, mx)
         #self.autoLevels = False  
         
-
+    def setSessionFolder(self, folder):
+        self.sessionFolder = folder
         
     def sleep(self,secs):
         """Asynchronous compatible sleep command. Sleeps for given time in seconds, but allows
