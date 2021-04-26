9adc4929745dc69bd98bc13dff231072d927b212
mserlin <marecserlin@gmail.com>
2021-04-26 11:31:12 -0700

    Add files via upload

--- Scanner GUI/Resources/nSOTScannerFormat.py
+++ Scanner GUI/Resources/nSOTScannerFormat.py
@@ -4,6 +4,14 @@
 import numpy as np
 from itertools import product
 
+siDict = {'T': 12, 'G' : 9, 'M' : 6, 'k' : 3, 'm' : -3, 'u' : -6, 'n' : -9, 'p' : -12}
+
+def get_key(val):
+    for key, value in siDict.viewItems(): #in python 3, switch to items.
+         if val == value:
+             return key
+    return "key doesn't exist"
+
 def formatNum(val, decimal_values = 2):
     if val != val:
         return 'nan'
@@ -12,60 +20,23 @@ def formatNum(val, decimal_values = 2):
     ind = string.index('e')
     num  = float(string[0:ind])
     exp = int(string[ind+1:])
-    if exp < -6:
-        diff = exp + 9
-        num = num * 10**diff
-        if num - int(num) == 0:
-            num = int(num)
-        else: 
-            num = round(num,decimal_values)
-        string = str(num)+'n'
-    elif exp < -3:
-        diff = exp + 6
-        num = num * 10**diff
-        if num - int(num) == 0:
-            num = int(num)
-        else: 
-            num = round(num,decimal_values)
-        string = str(num)+'u'
-    elif exp < 0:
-        diff = exp + 3
-        num = num * 10**diff
-        if num - int(num) == 0:
-            num = int(num)
-        else: 
-            num = round(num,decimal_values)
-        string = str(num)+'m'
-    elif exp < 3:
-        if val - int(val) == 0:
-            val = int(val)
-        else: 
-            val = round(val,decimal_values)
-        string = str(val)
-    elif exp < 6:
-        diff = exp - 3
-        num = num * 10**diff
-        if num - int(num) == 0:
-            num = int(num)
-        else: 
-            num = round(num,decimal_values)
-        string = str(num)+'k'
-    elif exp < 9:
-        diff = exp - 6
-        num = num * 10**diff
-        if num - int(num) == 0:
-            num = int(num)
-        else: 
-            num = round(num,decimal_values)
-        string = str(num)+'M'
-    elif exp < 12:
-        diff = exp - 9
-        num = num * 10**diff
-        if num - int(num) == 0:
-            num = int(num)
-        else: 
-            num = round(num,decimal_values)
-        string = str(num)+'G'
+    
+    #Get power of 3 index
+    si = exp-exp%3
+    if si < -12:
+        si = -12
+    elif si > 12:
+        si = 12
+
+    num = num * 10**(exp - si)
+    
+    if num - int(num) == 0:
+        num = int(num)
+    else: 
+        num = round(num,decimal_values)
+    
+    string = str(num)+get_key(si)
+    
     return string
     
 #By default, accepts no parent and will warn you for inputting a number without units. 
@@ -84,23 +55,9 @@ def readNum(string, parent, warningFlag = True):
                 return 'Rejected Input'
     except:
         try:
-            exp = string[-1]
-            if exp == 'm':
-                exp = 1e-3
-            if exp == 'u':
-                exp = 1e-6
-            if exp == 'n':
-                exp = 1e-9
-            if exp == 'p':
-                exp = 1e-12
-            if exp == 'k':
-                exp = 1e3
-            if exp == 'M':
-                exp = 1e6
-            if exp == 'G':
-                exp = 1e9
+            exp = siDict[string[-1]]
             try:
-                val = float(string[0:-1])*exp
+                val = float(string[0:-1])*10**exp
             except: 
                 return 'Incorrect Format'
         except:
@@ -108,7 +65,7 @@ def readNum(string, parent, warningFlag = True):
     return val
         
 #---------------------------------------------------------------------------------------------------------#         
-""" The following section creates a generic warning if a numebr is input without a unit."""
+""" The following section creates a generic warning if a number is input without a unit."""
         
 from PyQt4 import QtGui, QtCore, uic
 import sys
@@ -222,7 +179,7 @@ def processImageData(image, process):
         return image
         
 #---------------------------------------------------------------------------------------------------------#         
-""" The following section creates a better image plotted, based off of pyqt's ImageView, to allow plotting of partial images."""
+""" The following section creates a better image plotter, based off of pyqt's ImageView, to allow plotting of partial images."""
     
 import pyqtgraph as pg
 
