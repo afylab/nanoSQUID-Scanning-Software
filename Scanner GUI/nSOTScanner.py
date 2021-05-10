29113f9c80aed332e3632489530c93457ff32e45
Trevor Arp <trevorarp@ucsb.edu>
2021-05-10 10:38:34 -0700

    Added comment

--- Scanner GUI/nSOTScanner.py
+++ Scanner GUI/nSOTScanner.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt4 import Qt, QtGui, QtCore, uic
-import time 
+import time
 import ctypes
 myappid = 'YoungLab.nSOTScannerSoftware'
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
@@ -9,7 +9,7 @@ path = sys.path[0]
 sys.path.append(path + r'\Resources')
 sys.path.append(path + r'\ScanControl')
 sys.path.append(path + r'\LabRADConnect')
-sys.path.append(path + r'\nSOTCharacterizer')    
+sys.path.append(path + r'\nSOTCharacterizer')
 sys.path.append(path + r'\DataVaultBrowser')
 sys.path.append(path + r'\Plotters Control')
 sys.path.append(path + r'\TFCharacterizer')
@@ -24,6 +24,8 @@ sys.path.append(path + r'\SampleCharacterizer')
 sys.path.append(path + r'\GoToSetpoint')
 sys.path.append(path + r'\DeviceSelect')
 sys.path.append(path + r'\CoarseAttocubeControl')
+
+# I am adding something
 
 UI_path = path + r"\MainWindow.ui"
 MainWindowUI, QtBaseClass = uic.loadUiType(UI_path)
@@ -50,20 +52,20 @@ import exceptions
 
 class MainWindow(QtGui.QMainWindow, MainWindowUI):
     test = 0
-    """ The following section initializes, or defines the initialization of the GUI and 
+    """ The following section initializes, or defines the initialization of the GUI and
     connecting to servers."""
     def __init__(self, reactor, parent=None):
         """ nSOT Scanner GUI """
-        
+
         super(MainWindow, self).__init__(parent)
         self.reactor = reactor
         self.setupUi(self)
         self.setupAdditionalUi()
-        
+
         #Move to default position
         self.moveDefault()
-        
-        #Intialize all widgets. 
+
+        #Intialize all widgets.
         self.ScanControl = ScanControl.Window(self.reactor, None)
         self.LabRAD = LabRADConnect.Window(self.reactor, None)
         self.DeviceSelect = DeviceSelect.Window(self.reactor, None)
@@ -79,12 +81,12 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.GoToSetpoint = gotoSetpoint.Window(self.reactor, None)
         self.SampleCharacterizer = SampleCharacterizer.Window(self.reactor,None)
         self.AttocubeCoarseControl = CoarseAttocubeControl.Window(self.reactor,None)
-        
+
         #This module should always be initialized last, and have the modules
         #That are desired to be scriptable be input
         self.Scripting = Scripting.Window(self.reactor, None, self.ScanControl, self.Approach, self.nSOTChar, self.FieldControl, self.TempControl,
                                           self.SampleCharacterizer, self.GoToSetpoint)
-        
+
         #Connects all drop down menu button
         self.actionScan_Control.triggered.connect(self.openScanControlWindow)
         self.actionLabRAD_Connect.triggered.connect(self.openLabRADConnectWindow)
@@ -105,10 +107,10 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
 
         #Connectors all layout buttons
         self.push_Layout1.clicked.connect(self.setLayout1)
-        
+
         self.push_Logo.clicked.connect(self.toggleLogo)
         self.isRedEyes = False
-        
+
         #Connect signals between modules
         #When LabRAD Connect module emits all the local and remote labRAD connections, it goes to the device
         #select module. This module selects appropriate devices for things. That is then emitted and is distributed
@@ -116,10 +118,10 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.LabRAD.cxnLocal.connect(self.DeviceSelect.connectLabRAD)
         self.LabRAD.cxnRemote.connect(self.DeviceSelect.connectRemoteLabRAD)
         self.DeviceSelect.newDeviceInfo.connect(self.distributeDeviceInfo)
-        
+
         self.LabRAD.cxnDisconnected.connect(self.disconnectLabRADConnections)
         self.LabRAD.newSessionFolder.connect(self.distributeSessionFolder)
-        
+
         self.TFChar.workingPointSelected.connect(self.distributeWorkingPoint)
 
         self.nSOTChar.newToeField.connect(self.FieldControl.updateToeField)
@@ -127,41 +129,41 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.Approach.newPLLData.connect(self.ApproachMonitor.updatePLLPlots)
         self.Approach.newAux2Data.connect(self.ApproachMonitor.updateAux2Plot)
         self.Approach.newZData.connect(self.ApproachMonitor.updateZPlot)
-        
+
         self.Approach.updateFeedbackStatus.connect(self.ScanControl.updateFeedbackStatus)
         self.Approach.updateConstantHeightStatus.connect(self.ScanControl.updateConstantHeightStatus)
-        
+
         self.PosCalibration.newTemperatureCalibration.connect(self.setVoltageCalibration)
-        
+
         self.ScanControl.updateScanningStatus.connect(self.Approach.updateScanningStatus)
-        
-        #Make sure default calibration is emitted 
+
+        #Make sure default calibration is emitted
         self.PosCalibration.emitCalibration()
-        
+
         #Make sure default session flder is emitted
         self.LabRAD.newSessionFolder.emit(self.LabRAD.session_2)
-        
+
         #Open by default the LabRAD Connect Module and Device Select
         self.openLabRADConnectWindow()
         self.openDeviceSelectWindow()
-        
-        
+
+
     def setupAdditionalUi(self):
         """Some UI elements would not set properly from Qt Designer. These initializations are done here."""
         pass
-        
+
     #----------------------------------------------------------------------------------------------#
-            
+
     """ The following section connects actions related to default opening windows."""
-    
+
     def moveDefault(self):
         self.move(10,10)
-    
+
     def openScanControlWindow(self):
         self.ScanControl.showNormal()
         self.ScanControl.moveDefault()
         self.ScanControl.raise_()
-        
+
     def openLabRADConnectWindow(self):
         self.LabRAD.showNormal()
         self.LabRAD.moveDefault()
@@ -171,22 +173,22 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.nSOTChar.showNormal()
         self.nSOTChar.moveDefault()
         self.nSOTChar.raise_()
-            
+
     def openDataPlotter(self):
         self.PlottersControl.showNormal()
         self.PlottersControl.moveDefault()
         self.PlottersControl.raise_()
-            
+
     def openTFCharWindow(self):
         self.TFChar.showNormal()
         self.TFChar.moveDefault()
         self.TFChar.raise_()
-    
+
     def openApproachWindow(self):
         self.Approach.showNormal()
         self.Approach.moveDefault()
         self.Approach.raise_()
-            
+
     def openApproachMonitorWindow(self):
         self.ApproachMonitor.showNormal()
         self.ApproachMonitor.moveDefault()
@@ -196,12 +198,12 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.PosCalibration.showNormal()
         self.PosCalibration.moveDefault()
         self.PosCalibration.raise_()
-            
+
     def openFieldControlWindow(self):
         self.FieldControl.showNormal()
         self.FieldControl.moveDefault()
         self.FieldControl.raise_()
-            
+
     def openTempControlWindow(self):
         self.TempControl.showNormal()
         self.TempControl.moveDefault()
@@ -211,17 +213,17 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.Scripting.showNormal()
         self.Scripting.moveDefault()
         self.Scripting.raise_()
-        
+
     def openQRreaderWindow(self):
         self.QRreader.showNormal()
         self.QRreader.moveDefault()
         self.QRreader.raise_()
-        
+
     def openSetpointWindow(self):
         self.GoToSetpoint.showNormal()
         self.GoToSetpoint.moveDefault()
         self.GoToSetpoint.raise_()
-        
+
     def openDeviceSelectWindow(self):
         self.DeviceSelect.showNormal()
         self.DeviceSelect.moveDefault()
@@ -236,10 +238,10 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.AttocubeCoarseControl.showNormal()
         self.AttocubeCoarseControl.moveDefault()
         self.AttocubeCoarseControl.raise_()
-        
+
 #----------------------------------------------------------------------------------------------#
     """ The following section connects actions related to passing LabRAD connections."""
-    
+
     def distributeDeviceInfo(self,dic):
         #Call connectLabRAD functions for relevant modules
         self.PlottersControl.connectLabRAD(dic)
@@ -280,25 +282,25 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.SampleCharacterizer.updateDataVaultDirectory()
 
 #----------------------------------------------------------------------------------------------#
-            
+
     """ The following section connects signals between various modules."""
     def distributeWorkingPoint(self,freq, phase, channel, amplitude):
         self.Approach.setWorkingPoint(freq, phase, channel, amplitude)
-        
+
     def setVoltageCalibration(self,data):
         self.Approach.set_voltage_calibration(data)
         self.ScanControl.set_voltage_calibration(data)
 
 #----------------------------------------------------------------------------------------------#
-            
+
     """ The following section connects actions related to setting the default layouts."""
-        
+
     def setLayout1(self):
         self.moveDefault()
         self.hideAllWindows()
         self.openScanControlWindow()
         self.openApproachWindow()
-        
+
     def toggleLogo(self):
         if self.isRedEyes == False:
             self.push_Logo.setStyleSheet("#push_Logo{"+
@@ -310,7 +312,7 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
             "image:url(:/nSOTScanner/Pictures/SQUIDRotated2.png);background: black;}")
             self.push_Logo.setToolTip('')
             self.isRedEyes = False
-            
+
     def hideAllWindows(self):
         self.ScanControl.hide()
         self.LabRAD.hide()
@@ -325,7 +327,7 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
         self.TempControl.hide()
         self.SampleCharacterizer.hide()
         self.AttocubeCoarseControl.hide()
-            
+
     def closeEvent(self, e):
         try:
             self.disconnectLabRADConnections()
@@ -347,8 +349,8 @@ class MainWindow(QtGui.QMainWindow, MainWindowUI):
             self.GoToSetpoint.close()
         except Exception as inst:
             print inst
-    
-#----------------------------------------------------------------------------------------------#     
+
+#----------------------------------------------------------------------------------------------#
 """ The following runs the GUI"""
 
 if __name__=="__main__":
