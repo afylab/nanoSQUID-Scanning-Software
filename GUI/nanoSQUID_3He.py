f28df80b79f5ff46c6a9bd0a409708024cc8606c
Trevor Arp <trevorarp@ucsb.edu>
2021-07-14 15:39:11 -0700

    Initial Configuration on 3He

--- GUI/nanoSQUID_3He.py
+++ GUI/nanoSQUID_3He.py
@@ -15,7 +15,7 @@ class nanoSQUID_3He(nanoSQUIDSystem):
         dict = {'pll input':1, 'pll output':1, 'pid z out':1, 'z monitor':1, 'sum board toggle':1}
         self.equip.add_server("HF2LI Lockin", "hf2li_server", controller=CoreEquipment.HF2LI_Controller, config=dict)
 
-        # self.equip.add_server("ANC350", "anc350_server", controller=CoreEquipment.ANC350_Controller)
+        self.equip.add_server("ANC350", "anc350_server", controller=CoreEquipment.ANC350_Controller)
 
         dict = {'DC Readout':3, 'nSOT Bias':4, 'Noise Readout':2, 'nSOT Gate':1, 'Gate Reference':1, 'Bias Reference':4}
         self.equip.add_server("nSOT DAC", "dac_adc", "DA20_16_04 (COM6)", config=dict)
